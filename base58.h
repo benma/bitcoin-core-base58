ab324f64fc77593ab6aff1ead8efe36aca3023da
benma <mbencun@gmail.com>
2014-10-29 11:14:05 +0100

    Added class CBase58Data.
    Ported from https://github.com/bitcoin/bitcoin/blob/0.9.3/src/base58.h

--- base58.h
+++ base58.h
@@ -14,6 +14,7 @@
 #ifndef BITCOIN_BASE58_H
 #define BITCOIN_BASE58_H
 
+#include "allocators.h"
 #include <string>
 #include <vector>
 
@@ -63,5 +64,35 @@ bool DecodeBase58Check(const char* psz, std::vector<unsigned char>& vchRet);
  * vector (vchRet), return true if decoding is successful
  */
 bool DecodeBase58Check(const std::string& str, std::vector<unsigned char>& vchRet);
+
+/**
+ * Base class for all base58-encoded data
+ */
+class CBase58Data
+{
+protected:
+    // the version byte(s)
+    std::vector<unsigned char> vchVersion;
+
+    // the actually encoded data
+    typedef std::vector<unsigned char, zero_after_free_allocator<unsigned char> > vector_uchar;
+    vector_uchar vchData;
+
+    CBase58Data();
+    void SetData(const std::vector<unsigned char> &vchVersionIn, const void* pdata, size_t nSize);
+    void SetData(const std::vector<unsigned char> &vchVersionIn, const unsigned char *pbegin, const unsigned char *pend);
+
+public:
+    bool SetString(const char* psz, unsigned int nVersionBytes = 1);
+    bool SetString(const std::string& str);
+    std::string ToString() const;
+    int CompareTo(const CBase58Data& b58) const;
+
+    bool operator==(const CBase58Data& b58) const { return CompareTo(b58) == 0; }
+    bool operator<=(const CBase58Data& b58) const { return CompareTo(b58) <= 0; }
+    bool operator>=(const CBase58Data& b58) const { return CompareTo(b58) >= 0; }
+    bool operator< (const CBase58Data& b58) const { return CompareTo(b58) <  0; }
+    bool operator> (const CBase58Data& b58) const { return CompareTo(b58) >  0; }
+};
 
 #endif
